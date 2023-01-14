176d5eec739e224a3c61805e434b9e63b1edebf8
Dylan Garcia <50163127+nzx0@users.noreply.github.com>
2023-01-13 18:27:19 -0600

    Initial commit

---
+++ src/sh/dylan/dallemc/Main.java
@@ -0,0 +1,22 @@
+package sh.dylan.dallemc;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable(){
+        System.out.println("Starting up...");
+        //Bukkit.getPluginManager().registerEvents(new ChatListener(), this);
+
+    }
+
+    @Override
+
+    public void onDisable(){
+        System.out.println("Shutting down...");
+    }
+}
+
+
