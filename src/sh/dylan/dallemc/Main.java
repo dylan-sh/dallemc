70e6e652cbeab1e1d090ac230a3780432aa6a9f0
Dylan Garcia <50163127+nzx0@users.noreply.github.com>
2023-01-24 15:22:57 -0600

    god bless spigot forums for helping me resolve this

--- src/sh/dylan/dallemc/Main.java
+++ src/sh/dylan/dallemc/Main.java
@@ -1,12 +1,15 @@
 package sh.dylan.dallemc;
 
 import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
+    public static Plugin instance = null;
     @Override
     public void onEnable(){
         System.out.println("Dalle MC ready to rumble...");
+        instance = this;
         //Bukkit.getPluginManager().registerEvents(new ChatListener(), this);
         SuggestionDB suggestionDB = new SuggestionDB();
         VoteRecords voteRecords = new VoteRecords();
@@ -17,17 +20,17 @@ public class Main extends JavaPlugin {
         //timer section
         int delay = 0;
         int period = 2400;
-        Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
+        Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(instance, new Runnable() {
             @Override
             public void run() {
                 Bukkit.broadcastMessage("Voting has started!");
 
-                Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
+                Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(instance, new Runnable() {
                     @Override
                     public void run() {
                         Bukkit.broadcastMessage("10 seconds left to vote!");
 
-                        Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
+                        Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(instance, new Runnable() {
                             @Override
                             public void run() {
                                 Bukkit.broadcastMessage("Voting has finished!");
@@ -47,7 +50,7 @@ public class Main extends JavaPlugin {
     @Override
 
     public void onDisable(){
-        System.out.println("Shutting down DalleMC...");
+        instance = null; System.out.println("Shutting down DalleMC...");
     }
 }
 
