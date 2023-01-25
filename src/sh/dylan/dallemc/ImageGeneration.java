23c532abd13ba17a35ed6b685589c0cc77757273
Dylan Garcia <50163127+nzx0@users.noreply.github.com>
2023-01-24 22:13:05 -0600

    ok that worked way better than i expected

--- src/sh/dylan/dallemc/ImageGeneration.java
+++ src/sh/dylan/dallemc/ImageGeneration.java
@@ -2,8 +2,14 @@ package sh.dylan.dallemc;
 
 public class ImageGeneration {
 
+    private String apiKey;
+    private String suggestion;
+
     public ImageGeneration(){
         //this is where the api stuff happens.
+        //probably just going to call a python script with this tbh since it prefers that
     }
+
+
 
 }
