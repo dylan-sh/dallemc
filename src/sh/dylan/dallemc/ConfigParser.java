abf78eb22aa9661f05cedc8b664c9d443f25546f
Dylan Garcia <50163127+nzx0@users.noreply.github.com>
2023-01-24 20:07:16 -0600

    fixing some logic but probably breaking more things. this time it's so it doesn't throw an error when there's nobody on

---
+++ src/sh/dylan/dallemc/ConfigParser.java
@@ -0,0 +1,35 @@
+package sh.dylan.dallemc;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ConfigParser {
+    private Map<String, String> config = new HashMap<>();
+
+    public ConfigParser(String filePath) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                //split the line by the delimiter '='
+                String[] parts = line.split("=");
+                //if line doesn't contain '=' or is empty, skip it
+                if (parts.length != 2 || line.isEmpty()) {
+                    continue;
+                }
+                //trim the parts and put them in the map
+                config.put(parts[0].trim(), parts[1].trim());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("[FATAL] DalleMC is NOT configured. Please configure the txt in plugins/dallemc/config.txt. See README");
+        }
+    }
+
+    public String getValue(String key) {
+        return config.get(key);
+    }
+}
+
